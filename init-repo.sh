517702fd4271db5ce6b6ba284e904913329434d4
Michael Oultram <MichaelOultram@users.noreply.github.com>
2017-06-04 20:18:21 +0100

    Added an init script

---
+++ init-repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "${0%/*}" # cd into current repo
+
+# Download redox
+git submodule update --init
+cd redox
+
+# Remove kernel from redox
+git rm kernel
+
+# Make a link to kernel-distro
+ln -s ../kernel-distro kernel
+
+# Download the rest of redox
+git submodule update --init --recursive
