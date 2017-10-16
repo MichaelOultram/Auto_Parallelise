35164b91e9e3a1b7cabafd81178fae18d97ff0da
Michael Oultram <MichaelOultram@users.noreply.github.com>
2017-10-16 12:22:01 +0100

    Removed old single system image idea

--- example-programs/fibinacci_array/compiler_plugins/src/lib.rs
+++ example-programs/fibinacci_array/compiler_plugins/src/lib.rs
@@ -1,6 +1,10 @@
 #![feature(plugin_registrar, rustc_private, slice_patterns)]
+
+#[macro_use]
+extern crate rustc;
 extern crate syntax;
 extern crate rustc_plugin;
+
 use rustc_plugin::Registry;
 
 use syntax::ext::base::SyntaxExtension::{MultiModifier};
@@ -12,8 +16,13 @@ use roman_numerals::*;
 mod syntax_extension;
 use syntax_extension::*;
 
+mod lint_extension;
+use lint_extension::*;
+
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_macro("rn", expand_rn);
     reg.register_syntax_extension(Symbol::intern("para"), MultiModifier(Box::new(example_extension)));
+    reg.register_early_lint_pass(Box::new(LintExample{}));
+    //reg.register_late_lint_pass(Box::new(LintExample{}));
 }
