4307c4373126b70aebe23551c1ff24c06e74572c
Michael Oultram <michael@oultr.am>
2018-04-06 13:59:08 +0100

    Most complex generated tests results are added

--- src/playground/generated-test.sh
+++ src/playground/generated-test.sh
@@ -12,7 +12,7 @@ par_o=$(mktemp)
 stdin_params="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20"
 repetitions=1000
 error=0
-results_file=complex_results.csv
+results_file=most_complex_results.csv
 
 # Generate a sequential program
 sequential-program-generator $1 > ${tempfile}
--- src/playground/repeat_test.sh
+++ src/playground/repeat_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for complexity in {6..10}
+for complexity in {1..10}
 do
     for rep in {0..50}
     do
